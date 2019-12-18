43e382b99a89808b241e587af8ca0e1a17c638e1
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-12-18 23:50:16 +0100

    Add gif to wallpaper

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -23,7 +23,8 @@ export class SettingsComponent {
 
     listsChoiceImageForBackground : any =[
         ["https://images.unsplash.com/photo-1506202687253-52e1b29d3527?ixlib=rb-1.2.1&w=1000&q=80", "https://c4.wallpaperflare.com/wallpaper/4/562/548/dark-pattern-texture-wallpaper-thumb.jpg", "https://wallpaperplay.com/walls/full/b/6/5/14787.jpg", "https://cdn.wonderfulengineering.com/wp-content/uploads/2016/03/Water-wallpaper-12.jpg"],
-        ["https://img3.goodfon.com/wallpaper/nbig/7/88/devushka-bryunetka-model-foto-7149.jpg", "https://i.pinimg.com/originals/bf/16/70/bf16706a144f0de6793b501981c6a1a1.jpg", "https://static1.purepeople.com.br/articles/1/19/78/1/@/182124--950x0-2.jpg","https://jmd.im/wp-content/uploads/2017/06/blackiii_thumbnail.jpg"],
+        ["https://img3.goodfon.com/wallpaper/nbig/7/88/devushka-bryunetka-model-foto-7149.jpg", "https://i.pinimg.com/originals/bf/16/70/bf16706a144f0de6793b501981c6a1a1.jpg", "https://images.unsplash.com/photo-1483794344563-d27a8d18014e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80","https://jmd.im/wp-content/uploads/2017/06/blackiii_thumbnail.jpg"],
+        ["https://www.elsetge.cat/myimg/f/46-465549_cute-unicorn-wallpaper-engine.gif", "https://www.a2tech.eu/wp-content/uploads/2019/11/pikaaaaa-chuuuuuuuu.gif", "https://media1.tenor.com/images/8d8356c866266cef31ed2f2e119ffe58/tenor.gif?itemid=5552832", "https://thumbs.gfycat.com/DecimalThisDiscus-small.gif"]
     ]
 
     constructor(private globalService : GlobalService){}
