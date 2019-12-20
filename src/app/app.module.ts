b127a7a0fdc4e7ea87001024330156de5e59a31e
Paul TONDEREAU <paul.tondereau@yahoo.fr>
2019-12-20 14:17:18 +0100

    Début météo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ChangeDetectorRef } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service'
 
@@ -12,14 +13,24 @@ import { StatisticsComponent } from './features/statistics/statistics.component'
 import { TimeRemainingComponent } from './features/time-remaining/time-remaining.component';
 import { TimeRemainingCardComponent } from './features/time-remaining/time-remaining-card/time-remaining-card.component';
 import { CoffeeCounterComponent } from './features/coffee-counter/coffee-counter.component'
+import { WeatherComponent } from './features/weather/weather.component'
 
 
 import { GlobalService } from './global.service';
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MaterialModule ],
-  declarations: [ AppComponent, SettingsComponent, TimeComponent, StatisticsComponent,TimeRemainingComponent, TimeRemainingCardComponent, CoffeeCounterComponent ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MaterialModule, HttpClientModule ],
+  declarations: [ 
+      AppComponent,
+      SettingsComponent,
+      TimeComponent,
+      StatisticsComponent,
+      TimeRemainingComponent,
+      TimeRemainingCardComponent,
+      CoffeeCounterComponent,
+      WeatherComponent
+    ],
   bootstrap:    [ AppComponent ],
   providers: [ GlobalService, CookieService ]
 })
